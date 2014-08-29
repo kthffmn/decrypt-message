26cd2840fcefa78de35c28f0d269032e89c7744a
Katie Hoffman <ktahoffman@gmail.com>
2014-08-29 01:46:44 -0400

    complete spc for new code

--- spec/new_decode_spec.rb
+++ spec/new_decode_spec.rb
@@ -2,6 +2,42 @@ require 'pry'
 
 describe '#new_decode' do 
 
+  it 'rearranges and deletes characters in a string according the the rules in the readme' do
+    expect(new_decode("arpat")).to eq("rpta")
+    expect(new_decode("rawinbwo")).to eq("rainbow")
+    expect(new_decode("dermastoglyphsic")).to eq("dermatoglyphics")
+  end
+
+  it 'continues the process described in the spec above until no qualifying characters are left' do
+    expect(new_decode("roawinbow")).to eq("rainbow")
+    expect(new_decode("dermcatoglcypshsi")).to eq("dermatoglyphics")
+    expect(new_decode("cronquet-plagying")).to eq("croquet-playing")
+  end
+
+  it 'prioritizes pairs of characters that have the highest number of unique characters between them' do
+    expect(new_decode("amyblyidextlrous")).to eq("ambidextrously")
+  end
+
+  it 'removes undescores and any characters following the underscore' do
+    expect(new_decode("abc_def")).to eq("abc")
+  end 
+
+  it 'correctly calculates the final result of a very short string' do
+    expect(new_decode("aaccfgeb")).to eq("fgebac")
+  end
+
+  it 'correctly calculates the final result of a short string' do
+    expect(new_decode("abacbcbefge")).to eq("fgebac")
+  end
+
+  it 'correctly calculates the final result of a short string with one underscore' do
+    expect(new_decode('daccadfghd_i')).to eq("fgh")
+  end
+
+  it 'correctly calculates the final result of a string with multiple underscores' do
+    expect(new_decode("_a_abda_")).to eq("bda")
+  end
+
   it "correctly calculates the final result of a string of medium length" do
     message = "ttvmswxjzdgzqxotby_lslonwqaipchgqdo_yz_fqdagixyrobdjtnl_jqzpptzfcdcjjcpjjnnvopmh"
     expect(new_decode(message)).to eq("rainbow")
@@ -42,10 +78,9 @@ describe '#new_decode' do
     mid_point = Time.now
     new_decoded_string = new_decode(message)
     new_end = Time.now
-    expect(old_decoded_string).to eq("dragon")
     expect(new_decoded_string).to eq("dragon")
+    expect(old_decoded_string).to eq(new_decoded_string)
     expect((new_end - mid_point) * 70000).to be < (mid_point - old_start)
-
   end
 
 end
