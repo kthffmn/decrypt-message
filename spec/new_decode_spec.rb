56e837ad4d7346dd0c092c42cc1470a1cdc4336c
Katie Hoffman <ktahoffman@gmail.com>
2014-08-29 01:34:33 -0400

    add test for length where string is 400 char in length

--- spec/new_decode_spec.rb
+++ spec/new_decode_spec.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 describe '#new_decode' do 
 
   it "correctly calculates the final result of a string of medium length" do
@@ -5,16 +7,16 @@ describe '#new_decode' do
     expect(new_decode(message)).to eq("rainbow")
   end
 
-  it 'is 1.3x faster than #old_decode for 5 character strings' do 
+  it 'is 1.2x faster than #old_decode for 5 character strings' do 
     old_start = Time.now
     old_decode("arpat")
     mid_point = Time.now
     new_decode("arpat")
     new_end = Time.now
-    expect((new_end - mid_point) * 1.3).to be < (mid_point - old_start)
+    expect((new_end - mid_point) * 1.2).to be < (mid_point - old_start)
   end
 
-  it "is at least 1.3x faster than #decode for 16 character strings" do
+  it "is at least 1.3x faster than #old_decode for 16 character strings" do
     old_start = Time.now
     old_decode("dermastoglyphsic")
     mid_point = Time.now
@@ -23,7 +25,7 @@ describe '#new_decode' do
     expect((new_end - mid_point) * 1.3).to be < (mid_point - old_start)
   end
 
-  it "is at least 200x faster than #decode for 80 character strings" do
+  it "is at least 200x faster than #old_decode for 80 character strings" do
     message = "ttvmswxjzdgzqxotby_lslonwqaipchgqdo_yz_fqdagixyrobdjtnl_jqzpptzfcdcjjcpjjnnvopmh"
     old_start = Time.now
     old_decode(message)
@@ -31,6 +33,16 @@ describe '#new_decode' do
     new_decode(message)
     new_end = Time.now
     expect((new_end - mid_point) * 200).to be < (mid_point - old_start)
+  end
+
+  it "is at least 70,000x faster than #old_decode for 399 character strings" do
+    message = "ds_kwhlgtfoxxtsffndbiggplewknfojzhibcaamjwohnzqxxcmrizaacrtzgbxy__vff_wuu_qcbodxvwalrsjzzjjsaofuevqdnmkkpcdofsspfoaqrdewvuyjmmaxlsproiwkzzbiyswwpmkvtqshlayzcoeiuugacmxenhttlenbrkopiccpmxyntojgeeyvfthwuppyjbdkkbeucgtshhsjkowmbxpgttudovzijqyf_kxaeexnswjhp__hrwadbqlosmzvyggnutxebmvkasflwiwamuxevthpnryzfddxhcetvflwg__mkkqn_oxfulpiwhdmccnb__vtcybxpkdqserevktsdxncwgqoipbyhmujzfzmnxwpucheokdys_fstbqalji"
+    old_start = Time.now
+    old_decode(message)
+    mid_point = Time.now
+    new_decode(message)
+    new_end = Time.now
+    expect((new_end - mid_point) * 70000).to be < (mid_point - old_start)
   end
 
 end
